581c848ca195f9e8c44f34173d6f2958eb8a1459
ETG1 <ETG1@FNXWSF2-SHEL>
2022-09-13 17:57:24 +0530

    Add project files.

---
+++ APIDemo/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace APIDemo.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+
+
+    }
+}
