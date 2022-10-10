30b63a2fcc28aef76ccf179b79f56c32d3abccb4
ETG1 <ETG1@FNXWSF2-SHEL>
2022-10-10 09:39:53 +0530

    done

---
+++ PizzaClassLibrary/Pizza.cs
@@ -0,0 +1,16 @@
+﻿namespace PizzaClassLibrary.Models;
+
+public class Pizza
+{
+
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsGlutenFree { get; set; }
+
+    public PizzaSize Size {get; set;}
+}
+
+public enum PizzaSize
+{
+    Small, Medium, Large
+}
